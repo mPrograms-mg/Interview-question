684b164f71cdd3dae1458d96467000b1747fc5ca
Mahesh K <maheshkshirsagar128@gmail.com>
2025-12-16 17:02:04 +0530

    string to array different method

--- Problem14.js
+++ Problem14.js
@@ -2,8 +2,7 @@
 // let str = "Mahesh Kshirsagar"
 //  output = ["Mahesh, Kshirsagar"]
 
-let string = "Mahesh Kshirsagar";
-
+let string = "rohit rakesh sagar akash mahesh";
 //method 1
 let output = string.split(" ");
 // console.log(output);
@@ -11,14 +10,30 @@ let output = string.split(" ");
 // method 2
 let strArr = [];
 let word = "";
-let i = 0;
-for (let index = 0; index <= string.length; index++) {
-  if (index === string.length || string[index] === " ") {
-    strArr[i] = word;
-    i++;
+let index = 0;
+for (let i = 0; i <= string.length; i++) {
+  //   console.log("===>", string[i]);
+  if (i === string.length || string[i] === " ") {
+    strArr[index] = word;
+    index++;
     word = "";
   } else {
-    word += string[index];
+    word += string[i];
   }
 }
-console.log(strArr);
+// console.log(strArr);
+
+//method 3
+let strArray = [];
+let words = "";
+for (const ch of string) {
+  if (ch === " ") {
+    strArray.push(words);
+    words = "";
+  } else {
+    words += ch;
+  }
+}
+
+strArray.push(words);
+console.log(strArray);
