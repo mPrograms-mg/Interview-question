b1d13f14ba013c81a50f8ae14408dae2dc1e8f8a
Mahesh K <maheshkshirsagar128@gmail.com>
2025-12-18 23:53:00 +0530

    solved

--- Problem18.js
+++ Problem18.js
@@ -1,11 +1,9 @@
 // Count occurrences of each character in a string
-let str = "hello";
+let str = "aaabb";
 let occ = {};
+let ans;
 for (const element of str) {
-  console.log(element);
   occ[element] = (occ[element] || 0) + 1;
 }
 
 console.log(occ);
-
-// / Check if All Characters Have Equal Number of Occurrences
