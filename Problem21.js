9b6aa3ce0d23e995180dca9dce8fd9bdfc7abec7
Mahesh K <maheshkshirsagar128@gmail.com>
2025-12-25 23:58:07 +0530

    / Find the longest substring without repeating characters

--- Problem21.js
+++ Problem21.js
@@ -1,1 +1,21 @@
 // Find the longest substring without repeating characters
+
+var lengthOfLongestSubstring = function (str) {
+  let start = 0;
+  let ends = 0;
+  let maxLength = 0;
+  let subStr = new Set();
+
+  while (ends < str.length) {
+    if (!subStr.has(str[ends])) {
+      subStr.add(str[ends]);
+      maxLength = Math.max(maxLength, subStr.size);
+      ends++;
+    } else {
+      subStr.delete(str[start]);
+      start++;
+    }
+  }
+
+  return maxLength;
+};
