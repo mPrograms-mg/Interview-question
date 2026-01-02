1f1b1718a86ce406262f0b998177ca3e2caa5105
Mahesh K <maheshkshirsagar128@gmail.com>
2026-01-02 21:43:53 +0530

    Convert string to integer (atoi)

---
+++ Problem22.js
@@ -0,0 +1,27 @@
+//` Convert string to integer (atoi)`
+
+var myAtoi = function (s) {
+  str = s.trim();
+  if (!str) return 0;
+  let isPositive = 1;
+  let i = 0,
+    ans = 0;
+  if (str[i] === "+") {
+    isPositive = 1;
+    i++;
+  } else if (str[i] === "-") {
+    isPositive = 0;
+    i++;
+  }
+  for (; i < str.length; i++) {
+    let t = str.charCodeAt(i) - 48;
+    if (t > 9 || t < 0) break;
+    if (ans > 2147483647 / 10 || ans > (2147483647 - t) / 10) {
+      return isPositive ? 2147483647 : -2147483648;
+    } else {
+      ans = ans * 10 + t;
+    }
+  }
+  return isPositive ? ans : -ans;
+};
+console.log(myAtoi("42"));
